c64b7124b2247f8176ac181b2dfd250afdace955
mahailahmed <160250995+mahailahmed@users.noreply.github.com>
2024-03-05 12:34:31 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,53 @@
+var num1 = 5;
+var num2 = 5;
+console.log(num1 + num2);
+//simple addition
+var num3 = 10;
+var num4 = 15;
+var sum = num3 + num4;
+console.log(sum);
+//variable k sth print karana      (addition)
+var num5 = 10;
+var num6 = 15;
+var sub = num5 - num6;
+console.log(sub);
+//subtraction
+var num7 = 10;
+var num8 = 15;
+var multiply = num7 * num8;
+console.log(multiply);
+//multyply
+var num9 = 10;
+var num10 = 2;
+var div = num7 / num8;
+console.log(div); //divide
+var pencil = 21;
+var student = 5;
+var mod = pencil % student;
+console.log(mod);
+//module means persentage nikalna
+var a = 5;
+var b = 2;
+var power = Math.pow(a, b);
+console.log(power);
+//power nikalna for example 5power3 5*5*5=125
+var firstname = "manahil";
+var lastname = " ahmed";
+console.log(firstname + lastname); //2 name concatination
+var firstname1 = "manahil";
+var lastname2 = " ahmed";
+console.log(firstname1 + " " + lastname2); //agr space lana hoto +" "+  empty space
+var name1 = "manahil";
+var name2 = "ahmed";
+console.log(name1 - name2);
+var n = 5;
+n = n + 5;
+console.log(n);
+//previous n ki value  main 5 update hugya
+console.log(10 === 10); //type bhi match karata hy
+console.log(10 == 10); //type match nh karta
+console.log(10 != 10); //!= not equal to
+console.log(10 >= 5); //greaterthan
+console.log(10 <= 5); //smallerthan
+console.log(5 < 5);
+console.log(5 >= 5); //5 yato chota huga ya brabr huga dono main sayb 1 bhi sahe huga true huga
---
+++ main.ts
@@ -0,0 +1,95 @@
+const num1 = 5
+const num2 = 5
+console.log(num1+num2)
+//simple addition
+
+
+
+
+const num3 =10
+const num4 = 15
+const sum = num3 + num4
+console.log(sum)
+
+
+//variable k sth print karana      (addition)
+
+const num5 =10
+const num6 = 15
+const sub = num5 - num6
+console.log(sub)
+
+                             //subtraction
+
+
+
+
+
+const num7 =10
+const num8 = 15
+const multiply = num7 * num8
+console.log(multiply)
+                                 //multyply
+
+
+const num9 =10
+const num10 = 2
+const div = num7 / num8
+console.log(div)                       //divide
+
+
+const pencil = 21
+const student = 5
+ const mod = pencil % student
+ console.log(mod)
+
+                    //module means persentage nikalna
+
+
+
+ const a=5
+ const b=2
+ const power = a**b
+ console.log(power)
+
+                            //power nikalna for example 5power3 5*5*5=125
+
+
+
+
+
+const firstname ="manahil"
+const lastname=" ahmed"
+console.log (firstname+lastname)             //2 name concatination
+
+
+
+
+const firstname1="manahil"
+const lastname2=" ahmed"
+console.log (firstname1+" "+lastname2)                    //agr space lana hoto +" "+  empty space
+
+
+
+const name1="manahil"
+ const name2="ahmed"             
+ console.log(name1-name2)
+
+
+
+ let n=5
+ n=n+5
+ console.log(n)
+
+                        //previous n ki value  main 5 update hugya
+
+
+
+
+console.log(10===10)                                   //type bhi match karata hy
+        console.log(10==10)                           //type match nh karta
+        console.log(10!=10)                                //!= not equal to
+        console.log(10>=5)                                 //greaterthan
+        console.log(10<=10)                                  //smallerthan
+       console.log(5<5)
+       console.log(5>=5)                //5 yato chota huga ya brabr huga dono main sayb 1 bhi sahe huga true huga
